a0ec21b478a79f33175ab92ef109926ee82be36b
Mguy11 <0911554@hr.nl>
2018-12-17 11:53:51 +0100

    Quick fix voor MongoDB. Hoofdletter vergeten xD

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ if(process.env.ENV == 'Test'){
     db = mongoose.connect('mongodb://localhost/gamesAPI_test');
 }
 else{
-    db = mongoose.connect('mongodb://localhost/gamesAPI');
+    db = mongoose.connect('mongodb://localhost/GamesAPI');
 }
 var Game = require('./models/gameModel');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -30,5 +30,15 @@ app.get('/', function(req, res){
 app.listen(port, function(){
     console.log('Gulp is running the app on PORT: ' + port);
 });
+
+app.options("/api/games", function(req,res,next){
+    res.header('Accesa-Control-Allow-Origin', null);
+    res.header('Allow', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Header', 'Content-Type, Authorization, Content-Length, X-Request-Width');
+    res.send(200);
+  });
+
+
 
 module.exports = app;
